16921ed328be78810ea3c1a4a484939968be015d
ColleterVi <v_colleter@yahoo.fr>
2020-03-15 16:58:20 +0100

    NoteGeneration structure

--- src/app/sheet-rendering/NoteGeneration.ts
+++ src/app/sheet-rendering/NoteGeneration.ts
@@ -1,7 +1,62 @@
+import * as Vex from "vexflow";
+import { ReadVarExpr } from '@angular/compiler';
 export class NoteGeneration {
+    VF = Vex.Flow;
     private name: string;
     private hauteur : string;
     private sharp : string; 
-    note = ['a','b','c','d','e','f','g'];
+    
+    keys = ['a','b','c','d','e','f','g']; // keys
+    octave = [1, 2, 3, 4, 5, 6, 7, 8] // octave on keyboards
+    constructor(){}
+
+    generate_note(firstnote: string, pitch: string, flat_sharp:number, interval: number): string{
+
+
+        let firstNoteIndex : number = this.keys.indexOf(firstnote);
+        let nextNoteIndex : number = firstNoteIndex + this.random_interval(interval);
+        let nextNote : string;
+
+        //find random next keys based on interval and current keys
+        if (nextNoteIndex < 0) {
+            // If the key is some octave below
+
+            nextNote = this.keys[this.keys.length + nextNoteIndex];
+        } 
+        else if(nextNoteIndex >this.keys.length){
+            // Else if the key is an octave upper
+            nextNote = this.keys[nextNoteIndex- this.keys.length];
+        }
+        else {
+            // Else, it belongs to the same octave
+            nextNote = this.keys[nextNoteIndex];
+        }
+        
+        
+        return nextNote;
+
+
+    }
+
+    random_interval(interval:number){
+        let num = Math.floor(Math.random()*interval) + 1; // this will get a number between 1 and 99
+        num *= Math.floor(Math.random()*2) == 1 ? 1 : -1; // this will add minus sign in 50% of cases
+        return num
+    }
+
+    stringToStaveNote(keys:string[], duration: string, pitch: number){
+        //create VexFlow note
+        return new this.VF.StaveNote({keys:keys, duration:duration, octave_shift:pitch});
+    }
+
+    firstNote(minOctvave: number = 2, maxOctave: number = 6){
+        //slice is excusive so :
+        maxOctave +=1;
+
+        return this.keys[Math.floor(Math.random()*this.keys.length)] 
+        + '/' + 
+        this.octave.slice(minOctvave, maxOctave)[Math.floor(Math.random() * this.octave.slice(minOctvave, maxOctave).length)];
+    }
+    
 
 }
--- src/app/sheet-rendering/sheet-rendering.component.ts
+++ src/app/sheet-rendering/sheet-rendering.component.ts
@@ -8,21 +8,45 @@ import * as Vex from 'vexflow';
 })
 export class SheetRenderingComponent implements OnInit {
 
-  public VF; 
+  public VF = Vex.Flow;
   constructor() { }
 
   ngOnInit(): void {
-    this.VF = Vex.Flow;
+
     let div = document.getElementById("boo");
     let renderer = new this.VF.Renderer(div, this.VF.Renderer.Backends.SVG);
     renderer.resize(500,500);
     let context = renderer.getContext();
-    context.setFont("Arial",10,"");
+    context.setFont("Arial",10);
     
     let stave = new this.VF.Stave(10, 40, 400);
-
     stave.addClef("treble").addTimeSignature("4/4");
     stave.setContext(context).draw();
+    var notes = [
+      // A quarter-note C.
+      new this.VF.StaveNote({clef: "treble", keys: ["c/4"], duration: "q" }),
+    
+      // A quarter-note D.
+      new this.VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "q" }),
+    
+      // A quarter-note rest. Note that the key (b/4) specifies the vertical
+      // position of the rest.
+      new this.VF.StaveNote({clef: "treble", keys: ["b/4"], duration: "qr" }),
+    
+      // A C-Major chord.
+      new this.VF.StaveNote({clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "q" })
+    ];
+    
+    // Create a voice in 4/4 and add above notes
+    var voice = new this.VF.Voice({num_beats: 4,  beat_value: 4});
+
+    voice.addTickables(notes);
+    
+    // Format and justify the notes to 400 pixels.
+    var formatter = new this.VF.Formatter().joinVoices([voice]).format([voice], 400);
+    
+    // Render voice
+    voice.draw(context, stave);
   
   }
 
