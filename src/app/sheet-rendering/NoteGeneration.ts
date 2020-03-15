6cc5725cea5a17fe61409af057b99c6fa19c10a3
ColleterVi <v_colleter@yahoo.fr>
2020-03-15 18:41:56 +0100

    Generate keys v0

--- src/app/sheet-rendering/NoteGeneration.ts
+++ src/app/sheet-rendering/NoteGeneration.ts
@@ -1,61 +1,73 @@
 import * as Vex from "vexflow";
-import { ReadVarExpr } from '@angular/compiler';
-export class NoteGeneration {
+import { ReadVarExpr, NONE_TYPE } from '@angular/compiler';
+export class KeyGeneration {
     VF = Vex.Flow;
+    KEYS = ['a','b','c','d','e','f','g']; // KEYS
+    OCTAVE = [1, 2, 3, 4, 5, 6, 7, 8] // OCTAVE on keyboards
+
     private name: string;
     private hauteur : string;
     private sharp : string; 
     
-    keys = ['a','b','c','d','e','f','g']; // keys
-    octave = [1, 2, 3, 4, 5, 6, 7, 8] // octave on keyboards
     constructor(){}
 
-    generate_note(firstnote: string, pitch: string, flat_sharp:number, interval: number): string{
+    generateNextKey(currKey: string, currOctave: number, interval: number, flat_sharp:number=0 ): string{
 
+        let firstKeyIndex : number = this.KEYS.indexOf(currKey); // current key index (ie: 'c' -> 3)
+        let nextKeyIndex : number = firstKeyIndex + this.randomInterval(interval); // 
 
-        let firstNoteIndex : number = this.keys.indexOf(firstnote);
-        let nextNoteIndex : number = firstNoteIndex + this.random_interval(interval);
-        let nextNote : string;
+        let octaveDiff : number;
+        let keyDiff : number;
 
-        //find random next keys based on interval and current keys
-        if (nextNoteIndex < 0) {
-            // If the key is some octave below
+        let nextKey : string;
+        let nextOctave: number;
 
-            nextNote = this.keys[this.keys.length + nextNoteIndex];
+        octaveDiff = ~~(nextKeyIndex / this.KEYS.length);
+        keyDiff = nextKeyIndex % this.KEYS.length;
+        nextOctave = currOctave + octaveDiff;
+        //find random next KEYS based on interval and current key
+        if (nextKeyIndex < 0) {
+            // If the key is some OCTAVE below
+            nextKey = this.KEYS[this.KEYS.length + keyDiff];
         } 
-        else if(nextNoteIndex >this.keys.length){
-            // Else if the key is an octave upper
-            nextNote = this.keys[nextNoteIndex- this.keys.length];
+        else if(nextKeyIndex >this.KEYS.length){
+            // Else if the key is some OCTAVE upper
+            nextKey = this.KEYS[firstKeyIndex + keyDiff];
         }
         else {
-            // Else, it belongs to the same octave
-            nextNote = this.keys[nextNoteIndex];
+            // Else, it belongs to the same OCTAVE
+            nextKey = this.KEYS[nextKeyIndex];
         }
         
         
-        return nextNote;
-
+        return nextKey+"/"+nextOctave;
 
     }
 
-    random_interval(interval:number){
-        let num = Math.floor(Math.random()*interval) + 1; // this will get a number between 1 and 99
+    randomInterval(interval:number){
+        let num = Math.floor(Math.random()*interval) ; // this will get a number between 0 and 'interval'
         num *= Math.floor(Math.random()*2) == 1 ? 1 : -1; // this will add minus sign in 50% of cases
         return num
     }
 
-    stringToStaveNote(keys:string[], duration: string, pitch: number){
+    stringToStaveNote(keys:string[], duration: string,){
         //create VexFlow note
-        return new this.VF.StaveNote({keys:keys, duration:duration, octave_shift:pitch});
+        return new this.VF.StaveNote({keys:keys, duration:duration});
     }
 
-    firstNote(minOctvave: number = 2, maxOctave: number = 6){
+    firstKey(minOctvave: number = 2, maxOctave: number = 6){
         //slice is excusive so :
         maxOctave +=1;
-
-        return this.keys[Math.floor(Math.random()*this.keys.length)] 
+        let possibleOctave= this.OCTAVE.slice(minOctvave, maxOctave);
+        
+        return this.KEYS[Math.floor(Math.random()*this.KEYS.length)] 
         + '/' + 
-        this.octave.slice(minOctvave, maxOctave)[Math.floor(Math.random() * this.octave.slice(minOctvave, maxOctave).length)];
+        possibleOctave[Math.floor(Math.random() * possibleOctave.length)];
+    }
+
+    generateListNote(){
+        let firstKey = this.firstKey(3,5)
+        
     }
     
 
